e9fe405dc653e18d42c1c676880d59bb26cc61c5
SergioFerreiraFilho <106404822+SergioFerreiraFilho@users.noreply.github.com>
2022-10-04 10:35:16 -0300

    Adicionado Fonte

---
+++ excluir.js
@@ -0,0 +1,13 @@
+function acionarBotaoExcluir() {
+
+    btn_remove_all.classList.remove('d-none');
+}
+
+// let todosOsChecks = document.querySelectorAll('[data-check="acao"]')
+
+// todosOsChecks.forEach( (cadaCheck) => {
+//     cadaCheck.addEventListener('click', () => {
+//         acionarBotaoExcluir()
+//     })
+// });
+
